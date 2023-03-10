227a000153a8c8e8b2b9db49f49919a9c0e88cb5
Hernando Cardenas <h3rc4rm@gmail.com>
2023-03-10 14:28:15 +0100

    wip

--- routes/couple.routes.js
+++ routes/couple.routes.js
@@ -4,20 +4,44 @@ const Couple = require("../models/Couple.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 
-router.post("/new", isAuthenticated, (req, res, next) => {
-  const { user, coupleName } = req.body;
-  User.find(user)
-  .populate(coupleName)
-    .then(response => {
-      console.log(response)
-      res.json(response)
-    })
+const nuevo_id = User.findOne({ name: userName })
+.then(user => {
+  const userId = user._id;
+  console.log('User ID:', userId);
+})
+.catch(err => console.error(err));
 
-  Couple.create({ coupleName })
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => next(err))
+router.post("/new", isAuthenticated, (req, res, next) => {
+  const {users, coupleName, userName } = req.body;
+
+  User.findOne({ name: userName })
+.then(user => {
+  const userId = user._id;
+  console.log('User ID:', userId);
+  return userId
+})
+
+.then(userId=>{
+  Couple.create({$push:{users:user.id}})
+  res.json(userId);
+
+
+
+
+}
+  )
+
+  // .then(foundUsers => {
+  //   // Crear una instancia de pareja (couple) con los usuarios encontrados
+  //   const couple = new Couple({ users: foundUsers, coupleName });
+  //   return couple.save();
+   
+  // })
+  
+  .then(savedCouple => {
+    res.json(savedCouple);
+  })
+  .catch(err => next(err));
 });
 
 router.put("/edit/:idCouple", isAuthenticated, (req, res, next) => {
